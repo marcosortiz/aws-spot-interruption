91b8cd7de3c70b33f46e8bc29dae37f6688a2f8d
Marcos Ortiz <marcos.ortiz@icloud.com>
2019-05-19 16:47:27 -0500

    Formatting label color.

--- frontend/src/components/history/HistoryList.js
+++ frontend/src/components/history/HistoryList.js
@@ -3,6 +3,14 @@ import moment from 'moment';
 
 class SfrList extends React.Component {
 
+    statusLabelColor(status) {
+        if(status === 'termination_notified') {
+            return 'blue';
+        } else {
+            return '';
+        }
+    }
+
     renderContent() {
         return (
             <div>
@@ -19,17 +27,17 @@ class SfrList extends React.Component {
                 <tbody>
                     {this.props.events.map((e, i) => {
                         return (
-                            <tr key={i}>
+                            <tr key={i} >
                                 <td data-label="Timestamp">
                                     <div className="ui horizontal label small">{moment(e.Timestamp).format('HH:mm:ss')}</div>
                                 </td>
                                 <td>
-                                    <div className="ui horizontal label small blue">
+                                    <div className="ui horizontal label small">
                                         {e.EventType}
                                     </div>
                                 </td>
                                 <td>
-                                    <div className="ui horizontal label small blue">
+                                    <div className={`ui horizontal label small ${this.statusLabelColor(e.EventInformation.EventSubType)}`}>
                                         {e.EventInformation.EventSubType}
                                     </div>
                                 </td>
