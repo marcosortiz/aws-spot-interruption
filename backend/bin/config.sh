9f87ffa3390164f3e3373f2bfccf900f93ebf887
Marcos Ortiz <marcos.ortiz@icloud.com>
2019-05-22 09:46:04 -0500

    Changing backend deployment scripts.

--- backend/bin/config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-S3_BUCKET='spot-demo-sam'
-CFN_STACK_NAME_PREFIX='spot-demo-app'
--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,12 +1,13 @@
 # #!/bin/bash
 
-
 # Load config
 mydir="${0%/*}"
-source "$mydir"/config.sh
+REGION=$(cat "$mydir"/config.json | jq -r '.region')
+S3_BUCKET=$(cat "$mydir"/config.json | jq -r '.samS3BucketName')
+CFN_STACK_NAME_PREFIX=$(cat "$mydir"/config.json | jq -r '.cloudformationStackNamePrefix')
 
 # # Execute sam commands to deploy the package
 aws s3 mb s3://$S3_BUCKET
 sam build
-sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket $S3_BUCKET
-sam deploy --template-file packaged.yaml --stack-name $CFN_STACK_NAME_PREFIX --capabilities CAPABILITY_NAMED_IAM
+sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket $S3_BUCKET --region $REGION
+sam deploy --template-file packaged.yaml --stack-name $CFN_STACK_NAME_PREFIX --capabilities CAPABILITY_NAMED_IAM --region $REGION
