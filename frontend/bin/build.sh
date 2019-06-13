c6ed0607b83d0f5860df769115917bc907e60e7e
Marcos Ortiz <marcos.ortiz@icloud.com>
2019-06-13 11:56:28 -0500

    installing packages before calling node.

--- frontend/bin/build.sh
+++ frontend/bin/build.sh
@@ -4,7 +4,7 @@
 mydir="${0%/*}"
 export BACKEND_REGION=${REGION:-$(cat "$mydir"/../../config/config.json | jq -r '.region')}
 export CFN_STACK_NAME=${STACK_NAME:-$(cat "$mydir"/../../config/config.json | jq -r '.cloudformationStackName')}
-node "$mydir"/setup.js
 npm install
 npm install -g aws-sdk
+node "$mydir"/setup.js
 yarn build
